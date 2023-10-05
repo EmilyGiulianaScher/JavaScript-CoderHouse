01bf531b3d0ab9b3d2e12ba6d7a773885c263f57
GiulianaScher <emilygiulianascher@gmail.com>
2023-10-04 23:19:47 -0300

    2da parte proyecto CoderHouse

--- main.js
+++ main.js
@@ -1,85 +1,100 @@
 // Declaraciones de variables y objetos
 
-function Empanada(sabor, precio,) {
+function Empanada(sabor, precio) {
     this.sabor = sabor;
     this.precio = precio;
 }
 
- const empanadaCarne = new Empanada("Carne", 200);
- const empanadaPollo = new Empanada("Pollo", 175);
- const empanadaJyQ = new Empanada("Jamón y Queso", 150);
- const empanadaHumita = new Empanada("Humita", 180);
- const empanadaCyQ = new Empanada("Cebolla y Queso", 125);
+const empanadas = [new Empanada("Carne", 200), new Empanada("Pollo", 175), new Empanada("Jamón y Queso", 150), new Empanada("Humita", 180), new Empanada("Cebolla y Queso", 125)];
 
+let entrada;
 
 
 // Lógica y procesos
 
 alert("Bienvenido a la fábrica de empanadas! Aquí podrás elegir el tipo, la cantidad y el acompañamiento.");
-let entrada;
+
 
 do {
-    entrada = prompt("Ingresa 'continuar' (o cualquier letra) para empezar o 'finalizar' para cerrar el programa.");
+    entrada = prompt("Ingrese cualquier letra para empezar, o escriba 'finalizar' para cerrar el programa.");
     entrada = entrada.toLowerCase();
 
     if (entrada !== "finalizar") {
-
-        let cantidadTotal=0, resultado = 0, unidad=0;
+        let cantidadTotal = 0;
         let bebida;
 
-        let opcionValida = false;
-            let ingreso;
+        alert("Nuestras opciones son: Empanadas de Carne, Pollo, Jamón y Queso, Humita y Cebolla y Queso");
+        alert("Nuestros precios son: Carne: $200... Pollo: $175... Jamón y Queso: $150... Humita: $180... Cebolla y Queso: $125.");
+        alert("Tenemos un 10% de descuento si llevas más de 1 docena (12 empanadas)");
 
-       
-            alert("Nuestras opciones son: Empanadas de Carne, Empanadas de Pollo, Empanadas de Jamón y Queso, Empanadas de Humita y Empanadas de Cebolla y Queso");
-            
-            alert(" nuestros precios son: Carne: $200... Pollo: $175... Jamón y Queso: $150... Humita: $180... Cebolla y Queso: $125.")
-            alert("tenemos un 5% off si llevas mas de 1 docena (12 empanadas). Para finalizar el pedido de cantidad y sabor de empanadas, ingresar: continuar")
+        let pedido = [];
 
-            do {
-              sabor = prompt("elegi pue");
-              sabor = sabor.toLowerCase();
-                    sabor.push('');
-                    if (sabor == Carne) {
-                        cantidadTotal + 200;
-                    } else if (sabor == Pollo) {
-                        cantidadTotal +175;
-                    }else if (sabor == JyQ) {
-                        cantidadTotal +150;
-                    }else if (sabor == Humita) {
-                        cantidadTotal +180;
-                    }else if (sabor == CyQ) {
-                        cantidadTotal +125;
-                    }
-               precio += cantidadTotal;
-               unidad++;
-
-
-
-            } while (ingreso != "continuar");
-
-        function calculo(cantidadTotal, precio) {
-            return cantidadTotal * precio;
-        }
         while (true) {
-            bebida = prompt("¿Deseas agregar una bebida por el costo adicional de $100? Ingresa 'si' o 'no':");
+            let sabor = prompt("Elige el sabor de la empanada (Carne, Pollo, Jamón y Queso, Humita o Cebolla y Queso)  Para finalizar el pedido, ingresa: 'continuar':");
+            sabor = sabor.toLowerCase();
+
+            if (sabor === "continuar") {
+                break;
+            }
+
+            let empanadaElegida = empanadas.find(empanada => empanada.sabor.toLowerCase() === sabor);
+
+            if (empanadaElegida) {
+                let cantidad = parseInt(prompt("Ingresa la cantidad de empanadas de " + ${empanadaElegida.sabor} + "que deseas:"));
+                cantidadTotal += cantidad * empanadaElegida.precio;
+                pedido.push({ sabor: empanadaElegida.sabor, cantidad: cantidad });
+            } else {
+                alert("Sabor no válido. Por favor, elige un sabor de la lista.");
+            }
+        }
+
+        if (pedido.length >= 12) {
+            cantidadTotal *= 0.90; // Aplicar descuento del 10% si se compran más de 1 docena
+        }
+
+        while (true) {
+            bebida = prompt("¿Deseas agregar una bebida por el costo adicional de $300? Ingresa 'si' o 'no':");
             bebida = bebida.toLowerCase();
 
             if (bebida === "si") {
-                resultado = calculo(cantidadTotal, valor) + 100;
+                cantidadTotal += 300;
                 break;
             } else if (bebida === "no") {
-                resultado = calculo(cantidadTotal, valor);
                 break;
             } else {
                 alert("Respuesta no válida. Por favor, ingresa 'si' o 'no'.");
             }
         }
 
-        alert("Finalizado! El total de tu compra es de $" + resultado);
-    }
-} while (entrada !== "finalizar");
 
+        alert("Resumen del pedido:");
+
+        let resumenPedido = "Empanadas en tu pedido:\n";
+        for (const item of pedido) {
+            resumenPedido += ${item.cantidad} " empanadas de" + ${item.sabor}\n;
+        }
+
+        const precioTotalSinDescuento = cantidadTotal;
+        let descuento = 0;
+
+        if (pedido.length >= 12) {
+            descuento = precioTotalSinDescuento * 0.10; // Calcular el descuento del 10%
+        }
+
+        const precioFinalConDescuento = precioTotalSinDescuento - descuento;
+
+        const mensaje = `Resumen del pedido: 
+        Empanadas en tu pedido:\n${resumenPedido} 
+        Total sin descuento: $${precioTotalSinDescuento.toFixed(2)}
+        Descuento (10%): -$${descuento.toFixed(2)}
+        Precio final con descuento: $${precioFinalConDescuento.toFixed(2)}`;
+
+        alert(mensaje);
+
+
+
+    }
+} while (entrada !== "finalizar");
 
 
 
