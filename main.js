7a30907e6b177be9f202951b0601aed71620970c
GiulianaScher <emilygiulianascher@gmail.com>
2023-10-21 16:50:42 -0300

    3era Pre-Entrega

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
-localStorage.setItem('bienvenida', 'Bienvenido a Frizaditas!, alias las mejores empanadas...')
+localStorage.setItem('bienvenida', 'Bienvenido a Frizaditas!, alias las mejores empanadas...');
+
 const pedidos = [];
+
+function Empanada(sabor, precio) {
+    this.sabor = sabor;
+    this.precio = precio;
+}
+
+const sabores = [
+    new Empanada("carne", 200),
+    new Empanada("pollo", 175),
+    new Empanada("jamon y queso", 150),
+    new Empanada("humita", 180),
+    new Empanada("cebolla y queso", 125)
+];
 
 document.getElementById("guardarDatoBtn").addEventListener("click", guardarDato);
 document.getElementById("realizarFuncionBtn").addEventListener("click", realizarFuncion);
 
 function guardarDato() {
-    const nombre = document.getElementById("nombre").value;
+    const nombre = document.getElementById("nombre").value.toLowerCase();
     const cantidad = parseInt(document.getElementById("cantidad").value);
 
   
-    if (nombre.trim() !== "" && !isNaN(cantidad) && cantidad > 0) {
-       
-        pedidos.push({ nombre, cantidad });
+    const saborEncontrado = sabores.find(saborObj => saborObj.sabor === nombre);
 
-       
+    if (saborEncontrado && !isNaN(cantidad) && cantidad > 0) {
+        pedidos.push({ nombre, cantidad, precio: saborEncontrado.precio });
         document.getElementById("nombre").value = "";
         document.getElementById("cantidad").value = "";
-
-        alert("Pedido de " + cantidad + " empanada(s) de " + nombre + " agregado.");
+        alert(`Pedido de ${cantidad} empanada(s) de ${nombre} agregado a un costo de $${cantidad * saborEncontrado.precio}.`);
     } else {
-        alert("Ingresa un nombre de empanada y una cantidad válida.");
+        alert("Ingresa un sabor de empanada válido y una cantidad válida.");
     }
 }
 
